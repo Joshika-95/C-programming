82ceb82dc735f38c0408a5cc54d0b397b376748c
Joshika-95 <joshikasenthilkumar2007@gmail.com>
2026-02-16 23:49:21 +0530

    Add files via upload

---
+++ Nested For Loop/p14.c
@@ -0,0 +1,15 @@
+// A
+// BB
+// CCC
+// DDDD
+// EEEEE
+#include <stdio.h>
+int main() {
+    for (int i=1;i<=5;i++) {
+        for (int j=1;j<=i;j++) {
+            printf("%c",64+i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
