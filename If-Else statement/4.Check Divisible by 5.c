d07e4e7cc6784676a214e411ab77941b2ea79d0e
Joshika-95 <joshikasenthilkumar2007@gmail.com>
2026-02-16 23:32:35 +0530

    Add files via upload

---
+++ If-Else statement/4.Check Divisible by 5.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+int main() {
+    int num;
+    scanf("%d",&num);
+    if (num%5==0) {
+        printf("Divisible by 5");
+    }
+    else {
+        printf("Not divisible by 5");
+    }
+    return 0;
+}
+#include<stdio.h>
+int main() {
+    int num;
+    scanf("%d",&num);
+    if (num%5==0) {
+        printf("Divisible by 5");
+    }
+    else {
+        printf("Not divisible by 5");
+    }
+    return 0;
+}
